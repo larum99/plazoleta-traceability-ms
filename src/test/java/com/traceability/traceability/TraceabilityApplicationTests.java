0dd613658b49cdd2dd25daab6c334e6ecdd88a13
Laura <laura.barona@uao.edu.co>
2025-08-09 16:11:26 -0500

    Setup project

---
+++ src/test/java/com/traceability/traceability/TraceabilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.traceability.traceability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraceabilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
